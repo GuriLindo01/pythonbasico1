c03e7a526c931fbd8570886f999a0ae64ecc82da
Gustavo Vinicius Blazius <gustavo.blazius@escola.pr.gov.br>
2026-02-10 13:30:05 +0000

    teste 3

--- loginSimples.py
+++ loginSimples.py
@@ -1,20 +1,19 @@
 while True:
     nome = input("Digite seu nome: ").strip()
-    senha = input("Digite sua senha: ").strip()
 
     if nome =="":
         print("O nome não pode ser vazio. ")
-    else: 
+    else:
         break
 
 print("Nome: ", nome)
 
 while True:
-    senha = input("Digite sua senha: (max. 6 caracteres):")
+    senha = input("Digite sua senha: (max. 6 caractéres):")
 
-    if len(senha) <=6:
+    if len(senha) <= 6:
         break
     else:
-        print("A senha deve ter 6 caracteres no máximo. ")
+        print("A senha deve ter 6 caractéres no máximo. ")
 
-print("Conta registrada: ", senha)
+print(" Conta Registrada!: ", senha)
