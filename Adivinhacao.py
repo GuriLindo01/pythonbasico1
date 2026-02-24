af845fcfc836acfa4927a5b8795ad5bd7a0b9afa
Gustavo Vinicius Blazius <gustavo.blazius@escola.pr.gov.br>
2026-02-24 12:08:48 +0000

    versao02

---
+++ Adivinhacao.py
@@ -0,0 +1,35 @@
+
+import random
+
+print('***************************************')
+print('**********Jogo da adivinhação**********')
+print('***************************************')
+
+numero_secreto = random.randint(1, 100)
+total_tentativas = 5
+rodada = 1
+
+while (rodada <= total_tentativas):
+    chute_str = input("Digite o seu número de 1 a 100:")
+
+    print("Seu número é: ", chute_str)
+
+    chute = int(chute_str)
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+
+    if(acertou):
+        print("Você acertou!!")
+        break
+    else:
+        if(maior):
+            print("O seu chute foi maior que o número secreto")
+        elif(menor):
+            print("O seu chute foi menor que o número secreto")
+    rodada = rodada+1
+
+print(numero_secreto)
+print("Fim de jogo")
