cd1ad443db948601e9de46e2dcc80447ab741632
Gustavo Vinicius Blazius <gustavo.blazius@escola.pr.gov.br>
2026-02-24 12:32:48 +0000

    Versao03

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -10,11 +10,17 @@ total_tentativas = 5
 rodada = 1
 
 while (rodada <= total_tentativas):
+    
     chute_str = input("Digite o seu número de 1 a 100:")
 
-    print("Seu número é: ", chute_str)
-
     chute = int(chute_str)
+
+    if (chute < 1 or chute > 100):
+        print("O número deve ser entre 1 e 100")
+        continue
+
+    print("Tentativa {} de {}".format(rodada, total_tentativas))
+    print("Seu número é: ", chute_str)
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -29,7 +35,7 @@ while (rodada <= total_tentativas):
             print("O seu chute foi maior que o número secreto")
         elif(menor):
             print("O seu chute foi menor que o número secreto")
-    rodada = rodada+1
+        rodada = rodada+1
 
 print(numero_secreto)
 print("Fim de jogo")
