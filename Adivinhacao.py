9a940d29b988c5c7ad88aa0d597e5d5f50ea89a3
Gustavo Vinicius Blazius <gustavo.blazius@escola.pr.gov.br>
2026-02-24 13:05:31 +0000

    versao04

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -7,9 +7,23 @@ print('***************************************')
 
 numero_secreto = random.randint(1, 100)
 total_tentativas = 5
-rodada = 1
 
-while (rodada <= total_tentativas):
+print("Qual a dificuldade?")
+print("1(15 chances), 2(10 chances), 3(5 chances)")
+
+dificuldade = int(input("Defina o nível: "))
+
+if (dificuldade == 1):
+    total_tentativas = 15
+
+if (dificuldade == 2):
+    total_tentativas = 10
+
+elif (dificuldade == 3):
+    total_tentativas = 5
+
+for rodada in range(1, total_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_tentativas))
     
     chute_str = input("Digite o seu número de 1 a 100:")
 
@@ -19,7 +33,6 @@ while (rodada <= total_tentativas):
         print("O número deve ser entre 1 e 100")
         continue
 
-    print("Tentativa {} de {}".format(rodada, total_tentativas))
     print("Seu número é: ", chute_str)
 
     acertou = chute == numero_secreto
@@ -35,7 +48,6 @@ while (rodada <= total_tentativas):
             print("O seu chute foi maior que o número secreto")
         elif(menor):
             print("O seu chute foi menor que o número secreto")
-        rodada = rodada+1
 
 print(numero_secreto)
 print("Fim de jogo")
